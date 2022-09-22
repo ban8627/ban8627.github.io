cc25eb82293b4a4e590d436922a9ef55039bf735
banssa <ban8627@gmail.com>
2022-09-22 17:54:53 +0900

    0922 17:54

--- js/script.js
+++ js/script.js
@@ -1,25 +1,26 @@
+$(document).ready(function () {});
 window.onload = function () {
   AOS.init();
 
   // go-top
-  document.querySelector('.go-top > button').addEventListener('click', () => {
+  document.querySelector(".go-top > button").addEventListener("click", () => {
     window.scrollTo({
       top: 0,
       left: 0,
-      behavior: 'smooth'
+      behavior: "smooth",
     });
   });
 
   // contact-quick
-  let quickContact = document.querySelector('.quick-contact > a');
-  let conList = document.querySelector('.con-list');
-  quickContact.addEventListener('click', (event) => {
+  let quickContact = document.querySelector(".quick-contact > a");
+  let conList = document.querySelector(".con-list");
+  quickContact.addEventListener("click", (event) => {
     event.preventDefault();
-    conList.classList.toggle('active')
-  })
+    conList.classList.toggle("active");
+  });
 
   // portfolio all swiper
-  let pfSwiperthum = new Swiper('.sw-portfolio-thum', {
+  let allPFthum = new Swiper(".sw-portfolio-thum", {
     effect: "cube",
     grabCursor: true,
     cubeEffect: {
@@ -35,93 +36,107 @@ window.onload = function () {
     autoplay: {
       delay: 2500,
       disableOnInteraction: false,
-    }
-  })
-  let pfSwiperttxt = new Swiper('.sw-portfolio-txt', {
-    loop:true
-  })
-  pfSwiperthum.controller.control = pfSwiperttxt
+    },
+  });
+  let allPFLink = new Swiper(".sw-portfolio-txt", {
+    loop: true,
+    touchRatio: 0,
+    effect: "cube",
+    grabCursor: true,
+    cubeEffect: {
+      shadow: true,
+      slideShadows: true,
+      shadowOffset: 0,
+      shadowScale: 0,
+    },
+  });
+  allPFthum.controller.control = allPFLink;
+
+  const pofol = document.querySelectorAll(
+    ".sw-portfolio-thum",
+    "sw-portfolio-txt"
+  );
+  console.log(pofol);
 
   // portfolio study swiper
-  let swStudy = new Swiper('.sw-study-thum', {
+  let swStudy = new Swiper(".sw-study-thum", {
     loop: true,
     autoplay: {
       delay: 2500,
       disableOnInteraction: false,
-    }
-  })
-  let swStudyLink = new Swiper('.sw-study', {
+    },
+  });
+  let swStudyLink = new Swiper(".sw-study", {
     loop: true,
-  })
-  swStudy.controller.control = swStudyLink
-
+    touchRatio: 0,
+  });
+  swStudy.controller.control = swStudyLink;
 
   // portfolio vue.js swiper
-  let swVue = new Swiper('.sw-vue-thum', {
+  let swVue = new Swiper(".sw-vue-thum", {
     loop: true,
     autoplay: {
       delay: 2500,
       disableOnInteraction: false,
-    }
-  })
-  let swVueLink = new Swiper('.sw-vueclone', {
+    },
+  });
+  let swVueLink = new Swiper(".sw-vueclone", {
     loop: true,
-  })
-  swVue.controller.control = swVueLink
+    touchRatio: 0,
+  });
+  swVue.controller.control = swVueLink;
 
   // portfolio team swiper
-  let swTeam = new Swiper('.sw-team-thum', {
+  let swTeam = new Swiper(".sw-team-thum", {
     loop: true,
     autoplay: {
       delay: 2500,
       disableOnInteraction: false,
-    }
-  })
-  let swTeamLink = new Swiper('.sw-team-clone', {
+    },
+  });
+  let swTeamLink = new Swiper(".sw-team-clone", {
     loop: true,
-  })
-  swTeam.controller.control = swTeamLink
+    touchRatio: 0,
+  });
+  swTeam.controller.control = swTeamLink;
 
   // portfolio work swiper
-  let swWork = new Swiper('.sw-work-thum', {
+  let swWork = new Swiper(".sw-work-thum", {
     loop: true,
     autoplay: {
       delay: 2500,
       disableOnInteraction: false,
-    }
-  })
-  let swWorkLink = new Swiper('.sw-work-clone', {
+    },
+  });
+  let swWorkLink = new Swiper(".sw-work-clone", {
     loop: true,
-  })
-  swWork.controller.control = swWorkLink
+    touchRatio: 0,
+  });
+  swWork.controller.control = swWorkLink;
 
   // mbti & possible
-  const labels = [
-    '책임감',
-    '집중력',
-    '팀워크',
-    '끈기',
-    '전문성'
-  ];
+  const labels = ["책임감", "집중력", "팀워크", "끈기", "전문성"];
 
   const data = {
     labels: labels,
-    datasets: [{
-      backgroundColor: 'rgb(255, 255, 255,0.7)',
-      borderColor: 'rgb(0, 0, 0,0.5)',
-      title: {
-        display: false
+    datasets: [
+      {
+        backgroundColor: "rgb(255, 255, 255,0.7)",
+        borderColor: "rgb(0, 0, 0,0.5)",
+        title: {
+          display: false,
+        },
+        data: [95, 90, 95, 100, 85],
       },
-      data: [95, 90, 95, 100, 85],
-    }]
+    ],
   };
 
   const config = {
-    type: 'radar',
+    type: "radar",
     data: data,
     options: {
       elements: {
-        pointBorderWidth: 5
+        pointBorderWidth: 5,
       },
       scale: {
         min: 0,
@@ -132,20 +147,17 @@ window.onload = function () {
         maxTicksLimit: 10,
         ticks: {
           font: {
-            size: 16
-          }
+            size: 16,
+          },
         },
       },
       plugins: {
         legend: {
           display: false,
-        }
-      }
-    }
+        },
+      },
+    },
   };
 
-  const myChart = new Chart(
-    document.getElementById('myChart'),
-    config
-  );
+  const myChart = new Chart(document.getElementById("myChart"), config);
 };
